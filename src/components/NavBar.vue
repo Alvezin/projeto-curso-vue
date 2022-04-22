e995866c0c9569716b20d0107443371c436a16b5
Gabriel Alves <alvezinho.gabs@gmail.com>
2022-04-22 14:39:50 -0300

    modified:   public/favicon.ico 	new file:   public/img/burger.jpg 	new file:   public/img/logo.png 	modified:   src/App.vue 	new file:   src/components/Footer.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/NavBar.vue 	modified:   src/router/index.js 	deleted:    src/views/AboutView.vue 	modified:   src/views/HomeView.vue 	new file:   src/views/Pedidos.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav id="nav">
+        <router-link to="/" id="logo">
+            <img :src="logo" :alt="alt" id="logo_url">
+        </router-link>
+        <router-link to="/">Home</router-link>
+        <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'NavBar',
+        props: ['logo', 'alt']
+    }
+</script>
+
+<style scoped>
+    #nav{
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+    }
+    #nav #logo{
+        margin: auto;
+        margin-left: 0;
+    }
+    #logo_url{
+        width: 40px;
+        height: 40px;
+    }
+    #nav a{
+        color: #FCBA03;
+        text-decoration: none;
+        margin: 12px;
+        transition: .5s;
+    }
+    #nav a:hover{
+        color: white;
+    }
+
+</style>
