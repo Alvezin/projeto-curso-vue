7112da7944bfd0859c0579d7399fe8cc88466cf1
Gabriel Alves <alvezinho.gabs@gmail.com>
2022-04-23 14:30:21 -0300

    modified:   db/db.json 	modified:   package-lock.json 	modified:   src/components/BurgerForm.vue 	new file:   src/components/Message.vue

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,54 +1,135 @@
 <template>
   <div>
-      <p>componente de msg</p>
+      <Message :msg="state.msg" v-show="state.msg"/>
       
       <form id="burger-form">
           <div class="input-container">
                 <label class="customized" for="nome">Nome do Cliente: </label>
-                <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
+                <input type="text" id="nome" name="nome" v-model="state.nome" placeholder="Digite o seu nome">
           </div>
 
           <div class="input-container">
                 <label class="customized" for="pao">Escolha seu pão: </label>
-                <select name="pao" id="pao" v-model="pao">
+                <select name="pao" id="pao" v-model="state.pao">
                     <option value="">Selecione o seu pão</option>
-                    <option value="Integral">Integral</option>
+                    <option v-for=" pao in state.paes" :key="pao.id"
+                    :value="pao.tipo">{{pao.tipo}}</option>
                 </select>
           </div>
 
           <div class="input-container">
                 <label class="customized" for="carne">Escolha a carne: </label>
-                <select name="carne" id="carne" v-model="carne">
+                <select name="carne" id="carne" v-model="state.carne">
                     <option value="">Selecione a carne</option>
-                    <option value="maminha">Maminha</option>
+                    <option v-for="carne in state.carnes" :key="carne.id" :value="carne.tipo">{{carne.tipo}}</option>
                 </select>
           </div>
 
           <div class="input-container">
                 <label class="customized" for="opcionais">Selecione os opcionais: </label>
                 <div id="optional-container">
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame" :id="value">
-                        <label class="label-checkbox" for="salame">Salame</label>
+                    <div class="checkbox-container" v-for="opcoes in state.opcionaisData" :key="opcoes.id">
+                        <input type="checkbox" name="opcionais" v-model="state.opcionais" :value="opcoes.tipo" :id="opcoes.tipo">
+                        <label class="label-checkbox" :for="opcoes.tipo">{{opcoes.tipo}}</label>
                     </div>
                 </div>
           </div>
 
           <div class="submit-container">
-                <input type="submit" class="submit-btn" value="Criar meu burger!">
+                <input @click="preventReload($event),sendBurger()" type="submit" class="submit-btn" value="Criar meu burger!">
           </div>
       </form>
   </div>
 </template>
 
 <script>
+import { reactive } from '@vue/reactivity'
+import { onMounted } from '@vue/runtime-core'
+import Message from './Message.vue'
     export default {
-        name:'BurgerForm'
+        name:'BurgerForm',
+        components:{
+            Message
+        },
+        setup(){
+            const state = reactive({
+                paes: null,
+                carnes: null,
+                opcionaisData: null,
+                nome: null,
+                pao: null,
+                carne: null,
+                opcionais: [],
+                msg: null
+                
+            })
+
+            async function getIngredientes() {
+                const req = await fetch('http://localhost:3000/ingredientes')
+                const data = await req.json()
+                state.paes = data.paes
+                state.carnes = data.carnes
+                state.opcionaisData = data.opcionais    
+            }
+
+            function preventReload(e){
+                e.preventDefault()
+            }
+
+            function cleanInput(){
+                state.nome = null
+                state.pao = null
+                state.carne = null
+                state.opcionais = []
+            }
+
+            function appearMsg(id){
+                state.msg = `Pedido Nº ${id} Realizado com Sucesso`
+                setTimeout(()=>{
+                    state.msg = ''
+                },4000)
+            }
+
+            async function sendBurger(){
+                const data = {
+                    nome: state.nome,
+                    pao: state.pao,
+                    carne: state.carne,
+                    opcionais: [...state.opcionais],
+                    status: 'Solicitado'
+                }
+                const dataJson = JSON.stringify(data)
+
+                
+                const req = await fetch('http://localhost:3000/burgers', {
+                    method: 'POST',
+                    headers: {"Content-Type": "application/json"},
+                    body: dataJson
+                })
+
+                const res = await req.json()
+                cleanInput()
+                appearMsg(res.id)
+                
+            }
+
+            onMounted(()=>{
+                getIngredientes()
+            })
+            
+            return{
+                state,
+                getIngredientes,
+                preventReload,
+                sendBurger
+            }
+        }
+        
     }
 </script>
 
 <style scoped>
-    #burger-form{
+    #burger-form{  
         max-width: 400px;
         margin: 0 auto;
     }
